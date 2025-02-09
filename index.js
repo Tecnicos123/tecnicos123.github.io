f5345660a22554f09c59cbfcbcdf885f0e297663
tecnicostk <152911340+Tecnicos123@users.noreply.github.com>
2025-02-08 20:40:57 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let nombre = " "
+
+nombre = prompt ("Hola, Cual es tu nombre?")
+alert("Saludos " + nombre)
+alert("¿Qué sale de la cruza entre un mono y un pato?")
+alert("¡Un monopatín!")
+alert("¿Porqué vuelan los pájaros al sur?")
+alert("¡Porque caminando tardarían muchísimo!")
+alert("Fin")
